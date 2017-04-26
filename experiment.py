42020618000cd086c1bacf2284ce463e441356fb
oliver <loliverhennigh101@gmail.com>
2017-04-25 22:07:58 -0500

    changed plot a bit

--- experiment.py
+++ experiment.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
-import time
-time.sleep(5)
+for i in xrange(10):
+  for j in xrange(10000000):
+    blaa = j + 1
+  print("loss: " + str(i))
+  print("sdlkfj: 34")
+  print("sdlkfj: 34")
 print("it worked")
